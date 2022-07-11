52efb44df8b20ae6c852f472cc0835e79d651fd4
min9120 <a9120a@gmail.com>
2022-07-12 03:25:37 +0900

    feat: 커스텀 버튼 UI 컴포넌트 구현

---
+++ frontend/components/button/button.ts
@@ -0,0 +1,25 @@
+import styles from './button.styles.scss';
+
+class CustomButton extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    const button = document.createElement('button');
+    button.setAttribute('class', 'custom-button');
+    const style = document.createElement('style');
+    style.textContent = styles;
+    const type = this.getAttribute('type');
+    const text = this.getAttribute('text');
+    button.innerText = text ?? '';
+
+    if (type === 'primary') {
+      button.classList.add('primary');
+    } else if (type === 'normal') {
+      button.classList.add('normal');
+    }
+
+    this.shadowRoot?.append(style, button);
+  }
+}
+export default CustomButton;
