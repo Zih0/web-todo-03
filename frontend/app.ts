ed08dd51f9852c5d45a4f357cac16cd0e4b1a18b
Jiho Shin <ziho@kakao.com>
2022-07-12 11:29:02 +0900

    feat: 메인페이지 구현

--- frontend/app.ts
+++ frontend/app.ts
@@ -1,8 +1,12 @@
 import CustomButton from './components/button/button';
 import Card from './components/card/card';
+import KanvanBoard from './components/kavanBoard.ts/kanvanBoard';
+import MainPage from './pages/mainPage';
 import './style/style.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
   customElements.define('custom-button', CustomButton);
   customElements.define('todo-card', Card);
+  customElements.define('todo-kanvan', KanvanBoard);
+  customElements.define('main-page', MainPage);
 });
--- frontend/pages/mainPage/index.ts
+++ frontend/pages/mainPage/index.ts
@@ -1,8 +1,6 @@
 import Component from '../../components/core/component';
-import KanvanBoard from '../../components/kavanBoard.ts/kanvanBoard';
-import './style.scss';
 
-export default class MainPage extends Component<void, void> {
+export default class MainPage extends Component {
   constructor() {
     super();
   }
@@ -14,16 +12,8 @@ export default class MainPage extends Component<void, void> {
         <div id="main__kanvan__progress"></div>
         <div id="main__kanvan__done"></div>
         </div>
+        <custom-button type="primary" text="등록"></custom-button>
+        <todo-kanvan></todo-kanvan>
       `;
   }
-
-  setComponents() {
-    return {
-      main__kanvan__todo: new KanvanBoard(),
-      main__kanvan__progress: new KanvanBoard(),
-      main__kanvan__done: new KanvanBoard(),
-    };
-  }
 }
-
-customElements.define('main-page', MainPage);
