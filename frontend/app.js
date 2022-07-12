860435bf4a7f72499085bc9c110ef59167cddf2c
Jiho Shin <ziho@kakao.com>
2022-07-12 22:21:46 +0900

    chore: 칸반보드 오타 수정

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 import CustomButton from './components/button/button.js';
 import Card from './components/card/card.js';
-import KanvanBoard from './components/kavanBoard/kanvanBoard.js';
+import KanvanBoard from './components/kanvanBoard/kanvanBoard.js';
+import AlertModal from './components/modal/alertModal.js';
 import MainPage from './pages/mainPage/index.js';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,4 +9,5 @@ window.addEventListener('DOMContentLoaded', () => {
   customElements.define('todo-card', Card);
   customElements.define('todo-kanvan', KanvanBoard);
   customElements.define('main-page', MainPage);
+  customElements.define('alert-modal', AlertModal);
 });
