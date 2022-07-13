7bc40c565dc8565e326f32bac9ccbd49c293eed7
gyeongmin kim <a9120a@gamil.com>
2022-07-13 11:02:09 +0900

    feat: 메인 페이지 레이아웃 수정 및 프로젝트 헤더 추가 #23

--- frontend/pages/mainPage/index.js
+++ frontend/pages/mainPage/index.js
@@ -10,10 +10,16 @@ export default class MainPage extends Component {
     return `
         <div class="main__container">
           <todo-header text="Woowa! Todo"></todo-header>
-          <div class="kanvan__container">
-            <todo-kanvan id="main__kanvan__todo"></todo-kanvan>
-            <todo-kanvan id="main__kanvan__progress"></todo-kanvan>
-            <todo-kanvan id="main__kanvan__done"></todo-kanvan>
+          <div class="contents__container">
+            <div class="project-header">
+              <h1>Todo Project!</h1>
+              <p>신지호, 김경민이 함께하는 todo project 입니다. with vanilla JS, nodeJS, web component API ...</p>
+            </div>
+            <div class="kanvan__container">
+              <todo-kanvan id="main__kanvan__todo"></todo-kanvan>
+              <todo-kanvan id="main__kanvan__progress"></todo-kanvan>
+              <todo-kanvan id="main__kanvan__done"></todo-kanvan>
+            </div>
           </div>
         </div>
       `;
