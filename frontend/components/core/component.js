d44e2db1af181e5f3a90e310f47ef70f80b2bb5f
Jiho Shin <ziho@kakao.com>
2022-07-13 10:16:32 +0900

    feat: 컴포넌트 클래스 수정

--- frontend/components/core/component.js
+++ frontend/components/core/component.js
@@ -6,22 +6,60 @@ class Component extends HTMLElement {
 
     this.template = document.createElement('template');
     this.styles = document.createElement('style');
+
+    this.attachShadow({ mode: 'open' });
+    this.init();
+
+    this.reRender = this.render.bind(this);
   }
 
-  setStyle() {
-    return;
+  init() {
+    this.render();
+    this.addEvent();
   }
 
+  /*
+   * 각 컴포넌트에서 shadowDOM에 넣을 style을 삽입해줍니다.
+   */
+  setStyle() {}
+
+  /*
+   * 웹컴포넌트 innerHTML
+   */
   setTemplate() {
     return '';
   }
 
-  connectedCallback() {
-    this.attachShadow({ mode: 'open' });
-    this.template.innerHTML = this.setTemplate();
-    this.setStyle();
-
-    this.shadowRoot?.append(this.styles, this.template.content.cloneNode(true));
+  addEvent() {
+    return;
   }
+
+  render() {
+    this.template.innerHTML = this.setTemplate();
+  }
+
+  connectedCallback() {
+    this.setStyle();
+    this.shadowRoot?.append(this.styles, this.template.content.cloneNode(true));
+
+    this.setEvent();
+  }
+
+  /*
+   * DOM Unmount
+   */
+  disconnectedCallback() {}
+
+  /*
+   * attribute 구독
+   */
+  static get observedAttributes() {
+    return [];
+  }
+
+  /*
+   * 구독한 attribute가 변경되었을 때, Callback 처리
+   */
+  attributeChangedCallback(name, oldValue, newValue) {}
 }
 export default Component;
