50914a3d9e7757188dc5b0a1461476939fda4395
gyeongmin kim <a9120a@gamil.com>
2022-07-13 12:58:46 +0900

    chore: 주석제거

--- frontend/components/notification/notification.js
+++ frontend/components/notification/notification.js
@@ -30,9 +30,7 @@ class Notification extends Component {
     const author = '경민';
     const notification = '해야할 일에 컴포넌트 리팩터링을 등록하였습니다.';
     const time = '1분';
-    //현재 시간, 알림 생성 시간 비교해서 string 뽑아내는 로직
-    // notification 에 action, status, todo 만 강조
-    //notification action 에 따라 emogi 변경 로직
+
     return `
             <div class="noti-wrapper">
             <span class="emogi">${emogi}</span>
